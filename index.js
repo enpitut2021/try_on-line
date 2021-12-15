dd291e4b90979a02fd1cb806c60d9607bd335ea9
Yuku Kotani <yukukotani@gmail.com>
2021-12-15 12:45:28 +0900

    new model

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const shirt = new Shirt({
   width: 60,
 });
 
-
-
 const person = new Person({
   sex: "male",
   height: 170,
@@ -31,14 +29,13 @@ const person = new Person({
   inseam: 69,
 });
 
-
 height = 190;
-p = height/170;
-ctx.translate(110*1/3*(height-170)/10,-130*1/3*(height-170)/10);
-ctx.scale(1.0+(p-1)*0.3,1.0+p-1);//身長の変更
+p = height / 170;
+ctx.translate((((110 * 1) / 3) * (height - 170)) / 10, (((-130 * 1) / 3) * (height - 170)) / 10);
+ctx.scale(1.0 + (p - 1) * 0.3, 1.0 + p - 1); //身長の変更
 person.draw(ctx);
-q=0.085
-r=0.15
-ctx.translate(-34.5*q*10, -36*r*10);//服のサイズ変更の位置ずれ補正
-ctx.scale(1.0+q,1.0+r);
+q = 0.085;
+r = 0.15;
+ctx.translate(-34.5 * q * 10, -36 * r * 10); //服のサイズ変更の位置ずれ補正
+ctx.scale(1.0 + q, 1.0 + r);
 shirt.draw(ctx);
